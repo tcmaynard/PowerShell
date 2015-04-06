44c06e69753359bf517cba24b367252a1a8fa259
Tom Maynard <tmaynard3000@gmail.com>
2015-04-06 16:42:27 -0500

    Remove original function (sheesh)

--- Functions/AddToStartup.ps1
+++
@@ -1,5 +0,0 @@
-function AddToStartUp ($executable)
-{
-  Copy-Item $executable "C:\Users\Tom\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-#  Copy-Item $executable "C:\Users\Tom.000\AppData\Roaming\Microsoft\Windows\Start Menu\Startup\$executable"
-}
